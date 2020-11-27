52d4245fdd42521ff789adf2072b5cf26fa20729
Ramon Marcu <70721540+RamonMarcu@users.noreply.github.com>
2020-11-27 18:12:22 +0200

    Add files via upload

---
+++ Comparison Operators.js
@@ -0,0 +1,17 @@
+let hungerLevel = 7;
+if (hungerLevel > 7) {
+  console.log('Time to eat!')
+} else {
+  console.log('We can eat later!')
+}
+
+/* if run the first example console displays: We can eat later! */
+
+let hungerLevel = 7;
+if (hungerLevel < 8) {
+  console.log('Time to eat!')
+} else {
+  console.log('We can eat later!')
+}
+
+/* if run second example console displays: Time to eat! */
